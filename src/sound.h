4a79b50b8ff9bd2684c30214d1bd319d11bcc68e
shessel <shessel@users.noreply.github.com>
2019-02-21 23:49:01 +0100

    cleanup

---
+++ src/sound.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <cstdint>
+
+void sound_open_device(uint16_t sample_rate, uint16_t sample_bytes, uint16_t num_channels);
+void sound_close_device();
+void sound_queue_buffer(void* const buffer, uint32_t bytes);
--- src/synth.h
+++ src/synth.h
@@ -1,6 +1,14 @@
 #pragma once
-#include <cstdint>
 
-void open_device(uint16_t num_channels, uint16_t sample_rate, uint16_t sample_bytes);
-void close_device();
-void play_sound(void* const buffer, uint32_t bytes);
+struct ADSR
+{
+    float a;
+    float d;
+    float s;
+    float r;
+};
+
+void synth_init();
+void synth_deinit();
+void synth_generate(ADSR adsr);
+void synth_play();
